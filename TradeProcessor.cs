7480ae6d012a2b406ad6c55da4b1e60bb3bd73ec
Jim McGlone <jmcglone5572@sbcglobal.net>
2019-07-11 15:48:15 -0700

    Refactored ProcessTrades method for clarity.

--- TradeProcessor.cs
+++ TradeProcessor.cs
@@ -7,7 +7,16 @@ namespace SingleResponsibilityPrinciple
     public class TradeProcessor
     {
         private static float LotSize = 100000f;
+
         public void ProcessTrades(Stream stream)
+        {
+
+            var lines = ReadTradeData(stream);
+            var trades = ParseTrades(lines);
+            StoreTrades(trades);
+        }
+
+        private IEnumerable<string> ReadTradeData(Stream stream)
         {
             var lines = new List<string>();
 
@@ -15,65 +24,100 @@ namespace SingleResponsibilityPrinciple
             {
                 var line = string.Empty;
 
-                while((line = reader.ReadLine()) != null)
+                while ((line = reader.ReadLine()) != null)
                 {
                     lines.Add(line);
                 }
             }
 
+            return lines;
+        }
+
+        private List<TradeRecord> ParseTrades(IEnumerable<string> lines)
+        {
             var trades = new List<TradeRecord>();
 
             var lineCount = 1;
-            foreach(var line in lines)
+            foreach (var line in lines)
             {
                 var fields = line.Split(new char[] { ',' });
 
-                if(fields.Length != 3)
+                if(!ValidateTradeData(fields, lineCount))
                 {
-                    Console.WriteLine("WARN: Line{0} malformed. Only {1} field(s) found.", lineCount, fields.Length);
                     continue;
                 }
 
-                if(fields[0].Length != 6)
-                {
-                    Console.WriteLine("WARN: Trade currencies on Line {0} malformed: '{1}'", lineCount, fields[0]);
-                    continue;
-                }
-
-                var tradeAmount = 0;
-                if(!int.TryParse(fields[1], out tradeAmount))
-                {
-                    Console.WriteLine("WARN Trade amount on line {0} not a valid integer: '{1}'", lineCount, fields[1]);
-                }
-
-                decimal tradePrice;
-                if(!decimal.TryParse(fields[2], out tradePrice))
-                {
-                    Console.WriteLine("WARN: Trade price on line {0} no a valid decimal: '{1}'", lineCount, fields[2]);
-                }
-
-                var sourceCurrencyCode = fields[0].Substring(0, 3);
-                var destinationCurrencyCode = fields[0].Substring(3, 3);
-
-                var trade = new TradeRecord
-                {
-                    SourceCurrency = sourceCurrencyCode,
-                    DestinationCurrency = destinationCurrencyCode,
-                    Lots = tradeAmount / LotSize,
-                    Price = tradePrice
-                };
+                var trade = MapTradeDataToTradeRecord(fields);
 
                 trades.Add(trade);
                 lineCount++;
             }
 
+            return trades;
+        }
+
+        private TradeRecord MapTradeDataToTradeRecord(string [] fields)
+        {
+            var sourceCurrencyCode = fields[0].Substring(0, 3);
+            var destinationCurrencyCode = fields[0].Substring(3, 3);
+            var tradeAmount = int.Parse(fields[1]);
+            var tradePrice = decimal.Parse(fields[2]);
+
+            var trade = new TradeRecord
+            {
+                SourceCurrency = sourceCurrencyCode,
+                DestinationCurrency = destinationCurrencyCode,
+                Lots = tradeAmount / LotSize,
+                Price = tradePrice
+            };
+
+            return trade;
+        }
+
+        private bool ValidateTradeData(string[] fields, int currentLine)
+        {
+            if (fields.Length != 3)
+            {
+                Console.WriteLine("WARN: Line{0} malformed. Only {1} field(s) found.", currentLine, fields.Length);
+                return false;
+            }
+
+            if (fields[0].Length != 6)
+            {
+                Console.WriteLine("WARN: Trade currencies on Line {0} malformed: '{1}'", currentLine, fields[0]);
+                return false;
+            }
+
+
+
+            var tradeAmount = 0;
+            if (!int.TryParse(fields[1], out tradeAmount))
+            {
+                Console.WriteLine("WARN Trade amount on line {0} not a valid integer: '{1}'", currentLine, fields[1]);
+                return false;
+            }
+
+            decimal tradePrice;
+            if (!decimal.TryParse(fields[2], out tradePrice))
+            {
+                Console.WriteLine("WARN: Trade price on line {0} no a valid decimal: '{1}'", currentLine, fields[2]);
+                return false;
+            }
+
+
+
+            return true;
+        }
+
+        private void StoreTrades(List<TradeRecord> trades)
+        {
             using (var connection = new System.Data.SqlClient.SqlConnection("Data Source=(local); Initial Catalog=TradeDataBase; IntegratedSequrity=true"))
             {
                 connection.Open();
 
                 using (var transaction = connection.BeginTransaction())
                 {
-                    foreach(var trade in trades)
+                    foreach (var trade in trades)
                     {
                         var command = connection.CreateCommand();
                         command.Transaction = transaction;
