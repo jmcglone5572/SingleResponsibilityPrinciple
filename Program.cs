09f38c971c21899dbc6097dc5a65889e6c224868
Jim McGlone <jmcglone5572@sbcglobal.net>
2019-07-13 16:57:27 -0700

    Added concrete implementation and usage.

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,18 @@ namespace SingleResponsibilityPrinciple
     {
         static void Main(string[] args)
         {
+            var stream = new FileStream(@"D:\zMisc\SingleResponsibilityPrinciple\test.txt", FileMode.Open);
+
+            var tradeDataProvider = new StreamTradeDataProvider(stream);
+
+            var logger = new ConsoleLogger();
+            var tradeValidator = new SimpleTradeValidator(logger);
+            var tradeMapper = new SimpleTradeMapper();
+            var tradeParser = new SimpleTradeParser(tradeValidator, tradeMapper);
+            var tradeStorage = new DummyTradeStorage();
+            var tradeProcessor = new TradeProcessor(tradeDataProvider, tradeParser, tradeStorage);
+
+            tradeProcessor.ProcessTrades();
         }
     }
 }
